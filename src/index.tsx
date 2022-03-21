35307e6659a7931ca74ac272a6beac6ba47480c5
Дмитрий <qxwwxp@gmail.com>
2022-03-21 20:49:01 +0500

    feat(react): ✨ Реализован `ReactDOM`

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,33 @@ const React = {
   }
 }
 
+const ReactDOM = {
+  render (element, container) {
+    if (typeof element === 'string' || typeof element === 'number') {
+      container.appendChild(document.createTextNode(String(element)))
+      return
+    }
+
+    const { tag, props } = element
+    const domElement = document.createElement(tag)
+
+    if (props.children) {
+      props.children.forEach(child => {
+        ReactDOM.render(child, domElement)
+      })
+    }
+
+    for (const prop in props) {
+      if (prop !== 'children') {
+        const value = props[prop]
+        domElement[prop] = value
+      }
+    }
+
+    container.appendChild(domElement)
+  }
+}
+
 const App = () => (
   <div>
     <header>Header</header>
@@ -25,4 +52,4 @@ const App = () => (
   </div>
 )
 
-console.log(<App/>)
+ReactDOM.render(<App/>, document.getElementById('root'))
