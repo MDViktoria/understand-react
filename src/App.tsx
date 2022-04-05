b2297ae0652a6cfa94796456e73044a930f1849c
Дмитрий <qxwwxp@gmail.com>
2022-04-05 14:19:03 +0500

    refactor(app): ♻️ App вынесено в отдельный файл

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { React } from './react/react'
+import { Component } from '../@types/react'
+
+export const App: Component = () => (
+  <div>
+    <header>Header</header>
+    <main>
+      <h1>Page title</h1>
+      <p>lorem...</p>
+    </main>
+    <footer>Footer</footer>
+  </div>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,7 @@
-import { Component } from '../@types/react'
 import { React } from './react/react'
 import { ReactDOM } from './react/react-dom'
 
-const App: Component = () => (
-  <div>
-    <header>Header</header>
-    <main>
-      <h1>Page title</h1>
-      <p>lorem...</p>
-    </main>
-    <footer>Footer</footer>
-  </div>
-)
+import { App } from './App'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const rootContainer = document.getElementById('root')
+ReactDOM.render(<App/>, rootContainer)
